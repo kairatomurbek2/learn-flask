376148ec6b93c65484ee312bf82fb5071df2eedf
kairat <kairatomurbek2@gmail.com>
2019-01-20 02:22:10 +0600

    Learn send email

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,5 +14,7 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+mail = Mail(app)
 
 from app import routes, models
